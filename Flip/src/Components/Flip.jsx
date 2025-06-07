7c5279dcef574e62d942482776ee84a8b0c9639f
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-08 00:32:47 +0530

    Flip coin task

---
+++ Flip/src/Components/Flip.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Flip = () => {
+  return (
+    <>
+      <button className="coin">
+        Click here for the Toss
+      </button>
+      <div className="show-result">
+
+      </div>
+    </>
+  )
+}
+
+export default Flip
